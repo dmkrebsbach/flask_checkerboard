4458fa9cce1b92f457eb97bcd0a65a3767c2b5c4
dmkrebsbach <damiank@uw.edu>
2019-07-03 23:59:13 -0700

    updated html and css to reflect refactoring

--- checkerboard.py
+++ checkerboard.py
@@ -13,9 +13,9 @@ def index_x_y(x,y):
     return render_template("checkerboard.html", x=int(x), y=int(y))
 
 # Level 3 - Render a Checkerboard w/ x many rows and y many columns
-@app.route("/<int:n>/<int:x>/<color1>/<color2>")
-def index_x_y_color(n, x, color1, color2):
-    return render_template("checkerboard2.html", n = n, x = x, color1 = color1, color2 = color2)
+@app.route("/<x>/<y>/<color1>/<color2>")
+def index_x_y_color(x, y, color1, color2):
+    return render_template("checkerboard.html", x=int(x), y=int(y), color1 = color1, color2 = color2)
 
 ## code above this line    
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
